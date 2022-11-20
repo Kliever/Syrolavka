6a54f0ecf6ef9d003ec8bab751632db6c3f66b82
Kliever <mgrenady@gmail.com>
2022-11-21 00:33:11 +0200

    finish css header

--- src/js/modules/script.js
+++ src/js/modules/script.js
@@ -1,5 +1,22 @@
-export function isScript () {
+export function isScript() {
+
+  //drop=======================================================================
+
+  document.querySelectorAll('.js-drop-box').forEach((dropBox) => {
+    const dropBoxBtn = dropBox.querySelector('.js-drop-box__btn');
+    const dropBoxOpenBlock = dropBox.querySelector('.js-drop-box__open-block');
+
+    document.addEventListener('click', (event) => {
+      if (event.composedPath().includes(dropBoxBtn)) {
+        dropBoxBtn.classList.toggle('_active');
+        dropBoxOpenBlock.classList.toggle('_active');
+      } else if (!event.composedPath().includes(dropBoxOpenBlock)) {
+        dropBoxBtn.classList.remove('_active');
+        dropBoxOpenBlock.classList.remove('_active');
+      }
+    })
+
+  })
 
 
-  
 };
